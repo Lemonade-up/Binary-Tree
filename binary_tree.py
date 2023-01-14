63d31b26ec2a97e0be51111738c234036c037423
Wanjin Maglangit <wmaglangit1118@gmail.com>
2023-01-14 11:05:17 +0800

    added build tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,5 +1,6 @@
 class BinarySearchTreeNode:
     def __init__(self, data):
+        self.data = data
         self.left = None
         self.right = None
 
@@ -17,3 +18,30 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)    
+
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+        
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range( 1, len(elements)):
+        root.add_child(elements[i])
+        
+    return
+
+if __name__ == '__main__':
+    numbers = [19, 13, 19, 38, 1, 27, 8, 15, 16, 50, 12]
+    numbers_tree = build_tree([19, 13, 19, 38, 1, 27, 8, 15, 16, 50, 12])
+    print(numbers_tree)
